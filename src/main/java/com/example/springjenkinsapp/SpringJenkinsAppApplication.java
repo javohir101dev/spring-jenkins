9bb0414a23ef02c88449ad74917975fe9cf28259
Javohir Uralov <uralovjavohir25@gmail.com>
2022-08-07 14:58:34 +0500

    Logger added

--- src/main/java/com/example/springjenkinsapp/SpringJenkinsAppApplication.java
+++ src/main/java/com/example/springjenkinsapp/SpringJenkinsAppApplication.java
@@ -1,12 +1,24 @@
 package com.example.springjenkinsapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SpringJenkinsAppApplication {
 
+    public  static Logger logger = LoggerFactory.getLogger(SpringJenkinsAppApplication.class);
+
+    @PostConstruct
+    public void init(){
+        logger.info("Application started . . .");
+    }
+
     public static void main(String[] args) {
+        logger.info("Application executed . . .");
         SpringApplication.run(SpringJenkinsAppApplication.class, args);
     }
 
--- src/test/java/com/example/springjenkinsapp/SpringJenkinsAppApplicationTests.java
+++ src/test/java/com/example/springjenkinsapp/SpringJenkinsAppApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.springjenkinsapp;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SpringJenkinsAppApplicationTests {
 
+    public static Logger logger = LoggerFactory.getLogger(SpringJenkinsAppApplicationTests.class);
+
     @Test
     void contextLoads() {
+        logger.info("Test case is executing . . .");
+        assertTrue(true);
     }
-
 }
