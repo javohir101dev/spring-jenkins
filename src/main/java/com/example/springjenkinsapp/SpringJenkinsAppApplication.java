b58af5122c1945850ccbac61b0903aa93f07d8f2
Javohir Uralov <uralovjavohir25@gmail.com>
2022-08-07 14:56:11 +0500

    first commit

---
+++ src/main/java/com/example/springjenkinsapp/SpringJenkinsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjenkinsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJenkinsAppApplication.class, args);
+    }
+
+}
